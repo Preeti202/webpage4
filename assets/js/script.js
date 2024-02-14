ca8d7af4b65f0420522693d3c358fb68e2de5ba2
Preeti202 <pgaikwad.gaikwad4@gmail.com>
2024-02-14 11:24:57 +0530

    first comment

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+let formButton = document.querySelectorAll('button.formlink');
+//console.log(formButton);
+//for(var i=0;i<formButton.length;i++)
+{
+    formButton[i].addEventListener('click',function(){
+        var formButton = 
+        var formContent = document.querySelectorAll('.searchForm');
+        
+        formContent.classList.add('active');
+        this.classList.add('active');
+    })
+};//
